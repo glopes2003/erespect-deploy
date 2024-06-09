6bf7edce5ceddc5f3e9b7bf9a400064c2e95a7fd
Giovana <gicaalopes2003@gmail.com>
2024-06-09 20:46:30 -0300

    creating contacts modal

--- client/src/components/contatos.jsx
+++ client/src/components/contatos.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
 import psi from "../assets/psi.svg";
 import psi1 from "../assets/psi1.svg";
 import psi2 from "../assets/psi2.svg";
@@ -15,6 +17,19 @@ import link2 from "../assets/link2.svg";
 import link3 from "../assets/link3.svg";
 
 function Contatos() {
+  const [showModal, setShowModal] = useState(false);
+  const [currentMember, setCurrentMember] = useState(null);
+
+  const handleShow = (member) => {
+    setCurrentMember(member);
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    setCurrentMember(null); // Reset currentMember when modal is closed
+  };
+
   const contacts = [
     {
       category: "Psicólogos",
@@ -22,8 +37,8 @@ function Contatos() {
       members: [
         { name: "Adriana Lima", phone: "55+ (81) 98874-5555", image: psi1 },
         { name: "Matheus Cavalcanti", phone: "55+ (81) 99556-9887", image: psi2 },
-        { name: "Márcia Pereira", phone: "55+ (81) 99764-3442", image: psi3 }
-      ]
+        { name: "Márcia Pereira", phone: "55+ (81) 99764-3442", image: psi3 },
+      ],
     },
     {
       category: "Advogados",
@@ -31,8 +46,8 @@ function Contatos() {
       members: [
         { name: "Lucas Macedo", phone: "55+ (81) 99875-8862", image: adv1 },
         { name: "Sim Eun-seok", phone: "55+ (81) 98742-9982", image: adv2 },
-        { name: "Ricardo Almeida", phone: "55+ (81) 99886-1010", image: adv3 }
-      ]
+        { name: "Ricardo Almeida", phone: "55+ (81) 99886-1010", image: adv3 },
+      ],
     },
     {
       category: "Links Úteis",
@@ -40,9 +55,9 @@ function Contatos() {
       members: [
         { name: "Disque 100", phone: "100", image: link1 },
         { name: "Helpline da Safernet", image: link2 },
-        { name: "App Proteja Brasil", image: link3 }
-      ]
-    }
+        { name: "App Proteja Brasil", image: link3 },
+      ],
+    },
   ];
 
   return (
@@ -63,13 +78,11 @@ function Contatos() {
                   alt={contact.category}
                 />
                 <div className="card-body">
-                  <span className="badge rounded-pill px-3 py-2 my-pill">
-                    {contact.category}
-                  </span>
+                  <span className="badge rounded-pill px-3 py-2 my-pill">{contact.category}</span>
                 </div>
                 <ul className="list-group list-group-flush p-2">
-                  {contact.members.map((member, index) => (
-                    <li key={index} className="list-group-item d-flex flex-row">
+                  {contact.members.map((member, idx) => (
+                    <li key={idx} className="list-group-item d-flex flex-row">
                       <div className="py-2">
                         <img className="list-img" src={member.image} alt={member.name} />
                         <p className="d-flex flex-column justify-content-evenly align-items-start my-card-p">
@@ -105,6 +118,7 @@ function Contatos() {
                             className="btn btn-warning position-absolute border-0 rounded-pill my-card-btn text-light"
                             href="#"
                             role="button"
+                            onClick={() => handleShow(member)}
                           >
                             <i className="bi bi-envelope"></i>
                           </a>
@@ -118,6 +132,35 @@ function Contatos() {
           </div>
         </div>
       </div>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className="fw-medium fs-4 w-75">Entre em contato com {currentMember && currentMember.name}!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {currentMember && (
+            <Form>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Control type="text" placeholder="Nome" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Control type="email" placeholder="Email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formMessage">
+                <Form.Control
+                  type="textarea"
+                  as="textarea"
+                  rows={5}
+                  placeholder="Mensagem"
+                />
+              </Form.Group>
+              <Button className="btn btn-danger w-50 contacts-form-btn" type="submit">
+                Enviar
+              </Button>
+            </Form>
+          )}
+        </Modal.Body>
+      </Modal>
     </section>
   );
 }
