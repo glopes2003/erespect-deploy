aae70932ac8f90f98dba10c8b09b379893a3ed05
Giovana <gicaalopes2003@gmail.com>
2024-06-09 12:30:46 -0300

    implementing application's routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
-import ServiceFormPage from "./serviceFormPage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import ServiceFormPage from "./pages/serviceFormPage";
+import MainPage from "./pages/mainPage";
+import FeedbackForm from "./components/feedbackForm";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <>
-    <ToastContainer autoClose={3000} position="bottom-left" />
-      <ServiceFormPage />
-    </>
+    <Router>
+      <ToastContainer autoClose={3000} position="bottom-left" />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/service" element={<ServiceFormPage />} />
+        <Route path="/feedback" element={<FeedbackForm />} />
+      </Routes>
+    </Router>
   );
 }
 
